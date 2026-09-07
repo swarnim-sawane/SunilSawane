df5e8b60e5ef9435b4e27a0d4e4610a0a20c1a24
swarnim-sawane <swarnim.sawane@gmail.com>
2026-09-08 00:37:26 +0530

    chore: enable remote transfer for migration

--- backend/art-gallery-cms/config/server.ts
+++ backend/art-gallery-cms/config/server.ts
@@ -7,7 +7,7 @@ export default ({ env }) => ({
   },
   transfer: {
     remote: {
-      enabled: env.bool('REMOTE_DATA_TRANSFER_ENABLED', false),
+      enabled: env.bool('REMOTE_DATA_TRANSFER_ENABLED', true),
     },
   },
 });
